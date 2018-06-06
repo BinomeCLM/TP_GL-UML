3d56adc74765bede32ae1152a73f67813248f572
Lynn Ghandour <lghandour@insa-lyon.fr>
2018-06-06 19:11:01 +0200

    test 1.3

--- TP4_DevTest/AnalyseEmpreinte/TestApp/TestFonctionnelsBis.cpp
+++ TP4_DevTest/AnalyseEmpreinte/TestApp/TestFonctionnelsBis.cpp
@@ -1,10 +1,6 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include "FichierPatient.h"
-#include "FichEmpStream.h"
-#include "Dictionnaire.h"
-#include "Analyse.h"
-#include "FichEmpStream.h"
+#include "Main.h"
 #include <string>
 #include <iostream>
 #include <map>
@@ -18,19 +14,27 @@ namespace TestApp
 	public:
 		TEST_METHOD(test1_3)
 		{
+			Main main;
 			FichEmpStream * fstream = new FichEmpStream();
 
 			Dictionnaire dico = fstream->lireDictionnaire(dico, "./fichier_de_test/1.3/fichier_maladies_5.txt");
 			FichierPatient fp = fstream->lireFichierPatient("./fichier_de_test/1.3/fichier_empreintes_analyse_1.txt");
 
-			deque<Analyse> lesAnalyseReel;
-			
-			resultat = fp.analyserEmpreinte(dico);
+			deque<Analyse> lesAnalyseReel = fp.analyserEmpreinte(dico);
+			multimap<double, string> resultat;
+			resultat.insert(pair<double, string>(100, "Maladie6"));
+			resultat.insert(pair<double, string>(35, "Maladie1"));
 
-			Assert::AreEqual(analyseAttendu, resultat);
+			bool success;
+			if (resultat == main.retrouverResultat(lesAnalyseReel[0],false))
+			{
+				success = true;
+			}
+			Assert::IsTrue(success);
 
 
 
 
 		}
+	};
 }
